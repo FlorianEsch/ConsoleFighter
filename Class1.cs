564621ca35649ddee244133fe5afcc63fb298b98
FlorianEsch <80964146+FlorianEsch@users.noreply.github.com>
2021-03-19 17:50:46 +0100

    Update

---
+++ Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fightGame
+{
+    enum Action
+    {
+        atk,
+        def,
+        heal,
+        dft
+    }
+    class Fighter
+    {
+        string name;
+        public Action action;       
+        int live;
+        public bool checklive = true;
+     
+        public Fighter(string fighterName, int fighterLive) 
+        {
+            this.name = fighterName;
+            this.live = fighterLive;
+        }
+        public void Fighterhello()
+        {
+        Console.WriteLine("You are " + name + " you have " + live + "% Live.");
+        }
+        
+        public bool Alive { get { return live > 0; } }
+      
+        public void Livepad(int x)
+        {
+            live += x;
+        }
+       
+        public void Wapon()
+        {
+            Console.WriteLine("What is your action {0}?", name);
+            Console.WriteLine("You can choose between:");
+            Console.WriteLine("1: fight 2: defent 3: heal");
+            Console.WriteLine("You choose your action with the Number! {0}");
+            try
+            {
+                 string selectaction = Console.ReadLine();
+                int chosenAction = int.Parse(selectaction);
+            if (chosenAction == 1)
+            {
+                    action = Action.atk;
+                
+            }
+            else if (chosenAction == 2)
+            {
+                    action = Action.def;
+            } else
+            {
+                    action = Action.heal;
+            }
+            Console.Clear();
+             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+      
+        public void FighterEnde()
+        {
+            Console.WriteLine("You are " + name + " you have " + live + "% Live.");
+        }
+
+    }
+
+     
+}
+
+   
---
+++ Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace fightGame
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            // Name
+           
+
+            Console.WriteLine("Give Your Name Player 1:");
+           
+            Fighter Player1 = new Fighter(Console.ReadLine(),100);
+            Player1.Fighterhello();
+
+     
+            Console.WriteLine("Give Your Name Player 2:");
+          
+            Fighter Player2 = new Fighter(Console.ReadLine(),100);
+            Player2.Fighterhello();
+           
+
+
+
+            // Fight
+
+            do
+            {
+
+                //Choose Wapon
+              
+                Player1.Wapon();                    
+                Player2.Wapon();
+
+
+
+                if (Player1.action == Action.atk)
+                {
+                    if (Player2.action == Action.atk)
+                    {
+                      
+                        Player1.Livepad(-5);                   
+                        Player2.Livepad(-5);
+                        Console.WriteLine("strong fight");
+                    }
+                    else if (Player2.action == Action.def)
+                    {                     
+                        Player1.Livepad(-10);
+                        Console.WriteLine("Player1 get hit");
+                    }
+                    else
+                    {            
+                        Player2.Livepad(-10);
+                        Console.WriteLine("Player 2 get hit");
+                    }
+                }
+                if (Player1.action == Action.def)
+                {
+                    if (Player2.action == Action.atk)
+                    {                  
+                        Player2.Livepad(-10);
+                        Console.WriteLine("Player 2 get hit");
+                    }
+                    else if (Player2.action == Action.def)
+                    {
+                        Console.WriteLine("all defensiv");
+                    }
+                    else 
+                    {                     
+                        Player2.Livepad(10);
+                        Console.WriteLine("Player 2 heal");
+                    }
+                }
+                if (Player1.action == Action.heal)
+                
+                {
+                    if (Player2.action == Action.atk)
+                    {                      
+                        Player1.Livepad(-10);
+                        Console.WriteLine("Player1 get hit");
+                    }
+                    else if (Player2.action == Action.def)
+                    {                   
+                        Player1.Livepad(10);
+                        Console.WriteLine("Player 1 heal");
+                    }
+                    else
+                    {                     
+                        Player1.Livepad(10);
+                        Player2.Livepad(10);
+
+                        Console.WriteLine("Player 1 heal");                      
+                        Console.WriteLine("Player 2 heal");
+                    }
+                }
+                
+                Player1.FighterEnde();              
+                Player2.FighterEnde();       
+                
+            } while (Player1.Alive && Player2.Alive);
+
+
+
+
+            
+            Player1.FighterEnde();           
+            Player2.FighterEnde();
+
+
+
+
+            
+
+
+        }
+
+
+
+    }
+}
